ec655ed22822e4a3c87232a319a90a2505c428f4
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-09-03 15:50:58 +0900

    Home & Search - axios 추가 - ProductList 컴포넌트 추가 및 Home과 search에 적용

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,30 @@
+import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import { useRouter } from "next/router";
+import axios from "@/lib/axios";
+import { useEffect, useState } from "react";
 
 export default function Search() {
+  const [products, setProducts] = useState([]);
   const router = useRouter();
   const { q } = router.query;
+
+  const getProducts = async (query) => {
+    const res = await axios.get(`products/?q=${query}`);
+    const nextProducts = res.data.results ?? [];
+    setProducts(nextProducts);
+  };
+
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
+
   return (
     <>
       <h1>Search</h1>
       <SearchForm defaultValue={q} />
       <div>Search {q}</div>
+      <ProductList products={products} />
     </>
   );
 }
