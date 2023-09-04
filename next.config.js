931ec539815ab67a38191b26f62224a5dd4144ea
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-09-04 13:13:38 +0900

    Redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // redirect (주소가 변경되었을 경우 이전 주소를 자동으로 새 주소로 이동시킴)
+  async redirects() {
+    return [
+      {
+        source: "/products/:id", // redirect 처리 할 주소
+        destination: "/items/:id", // 목적지
+        permanent: false, // 브라우저의 주소 저장 여부
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
