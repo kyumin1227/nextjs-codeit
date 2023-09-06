483384fdcddfd4bc6c06d54e6437617efb435966
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-09-06 13:00:36 +0900

    Head component

--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import SearchForm from "@/components/SearchForm";
 import { useRouter } from "next/router";
 import axios from "@/lib/axios";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 
 export default function Search() {
   const [products, setProducts] = useState([]);
@@ -21,6 +22,9 @@ export default function Search() {
 
   return (
     <>
+      <Head>
+        <title>{q} 검색 결과</title>
+      </Head>
       <h1>Search</h1>
       <SearchForm defaultValue={q} />
       <div>Search {q}</div>
