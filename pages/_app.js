6591e22210f93e255a474420674907b07d78c36f
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-09-07 13:27:39 +0900

    Font apply

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,12 @@ import Header from "@/components/Header";
 import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/global.css";
 import Head from "next/head";
+import { Noto_Sans_KR } from "next/font/google";
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ["400", "700"],
+  subsets: [],
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,6 +16,11 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Codeitmall</title>
         <link rel="icon" href="/product.jpeg" />
+        <style>{`
+          html {
+            font-family: ${notoSansKR.style.fontFamily}, sans-serif;
+          }
+        `}</style>
       </Head>
       <ThemeProvider>
         <Header />
