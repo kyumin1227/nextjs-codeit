f2dec4298112f6c72ea04196aa0c137d2e86e88b
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-09-12 15:41:19 +0900

    Update short-link api - PATCH method - DELETE method

--- pages/api/short-links/[id].js
+++ pages/api/short-links/[id].js
@@ -5,9 +5,20 @@ export default async function handler(req, res) {
   await dbConnect();
   const { id } = req.query;
   switch (req.method) {
+    case "PATCH":
+      // findByIdAndUpdate의 세번째 param은 option(new는 반환값을 변경된 값으로 설정)
+      const updatedShortLink = await ShortLink.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.send(updatedShortLink);
+      break;
     case "GET":
       const shortLink = await ShortLink.findById(id);
       res.status(200).send(shortLink);
+      break;
+    case "DELETE":
+      await ShortLink.findByIdAndDelete(id);
+      res.status(204).send("Delete");
       break;
   }
 }
